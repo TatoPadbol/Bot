4d1221c375cd104a9fd5a0b68a4d5bf22190e50e
TatoPadbol <tatopadbol@gmail.com>
2025-05-09 18:00:35 -0300

    Add files via upload

--- pages/admin.js
+++ pages/admin.js
@@ -37,8 +37,7 @@ export default function AdminClient() {
   };
 
   return (
-    <div className="form-container" style={{ maxWidth: 500, margin: 'auto', display: 'flex', flexDirection: 'column', gap: 10 }}>
-      <h1 style={{ color: 'red', textAlign: 'center' }}>PRUEBA</h1>
+    <div className="form-container">
       <input placeholder="Nombre del cliente" value={client.name} onChange={(e) => handleChange("name", e.target.value)} />
       <input placeholder="Rubro" value={client.industry} onChange={(e) => handleChange("industry", e.target.value)} />
       <select value={client.country} onChange={(e) => handleChange("country", e.target.value)}>
