bd90dc1245733d18fef6dda80c9553a6ca8e3f65
TatoPadbol <tatopadbol@gmail.com>
2025-05-15 14:56:26 -0300

    Admin

--- pages/admin.js
+++ pages/admin.js
@@ -13,7 +13,7 @@ export default function Admin() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const res = await fetch("/api/clientes", {
+    const res = await fetch("/api/save-client", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ name, industry, country, phone, info, url }),
